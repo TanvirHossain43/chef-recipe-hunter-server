f5708d59d595402cc004bebd171f74f592635979
Tanvir hossain <tanvirhossain4393@gamil.com>
2023-05-02 23:31:28 +0600

    chef data added

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const app = express();
+// const cors = require('cors')
 const port = process.env.PORT || 5000;
 
+const chefData = require('./data/chef.json')
+
+
+// app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('chef recipe hunter server is ok ')
 });
 
+app.get('/chefdata', (req, res) => {
+    res.send(chefData)
+});
+
 app.listen(port, () => {
-console.log(`chef ApI is running on Port:${port}`)
+    console.log(`chef ApI is running on Port:${port}`)
 })
