568c90ed069f59a794428b855896f21635486c9d
Tanvir hossain <tanvirhossain4393@gamil.com>
2023-05-03 02:54:35 +0600

    picture added into the chef data json file

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
-// const cors = require('cors')
+const cors = require('cors')
 const port = process.env.PORT || 5000;
 
 const chefData = require('./data/chef.json')
 
 
-// app.use(cors());
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('chef recipe hunter server is ok ')
